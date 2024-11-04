5654932297f166af79785a097279109ab7f1cec5
VictorValdespino <vvaldespino99@gmail.com>
2024-11-04 17:51:41 -0600

    ch

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 #Respuesta HTTP
 #from django.http import HttpResponse
 # Create your views here.
@@ -42,3 +42,28 @@ def signup(request):
 
 def tasks(request):
     return render(request,'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{
+            'form':AuthenticationForm
+        })
+    else:
+        #valida si el usuario esta autenticado usando authenticate
+        user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+        #valida 
+        if user is None:
+            return render(request,'signin.html',{
+            'form':AuthenticationForm,
+            'error':'Username or password is incorrect'
+            })
+        else:
+            #Guardar cookis de inicio de sesion
+            login(request, user)
+            return redirect('tasks')
+        
+        
